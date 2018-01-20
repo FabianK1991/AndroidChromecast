d2ac9a72c906b61d939c58fbb64e148e4b6f9b05
Fabian <fab.kramm@googlemail.com>
2018-01-20 12:07:46 +0100

    Working example + volley http request

--- sample-app/src/main/java/io/reactivex/android/samples/MainActivity.java
+++ sample-app/src/main/java/io/reactivex/android/samples/MainActivity.java
@@ -18,6 +18,15 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -47,6 +56,29 @@ public class MainActivity extends Activity {
     }
 
     void onRunSchedulerExampleButtonClicked() {
+        final TextView mTextView = findViewById(R.id.textView);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
         disposables.add(sampleObservable()
             // Run on a background thread
             .subscribeOn(Schedulers.io())
